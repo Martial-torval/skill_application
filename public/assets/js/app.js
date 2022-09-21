344084449e5b00b23877426e96856507c30bd1c7
Younes Zenasni <youneszenasni21@gmail.com>
2022-09-21 14:42:39 +0200

    modal

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -54,3 +54,81 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("webdesign");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+var modal2 = document.getElementById("myModal2");
+var btn = document.getElementById("devweb");
+var span = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+  }
+
+
+var modal3 = document.getElementById("myModal3");
+var btn = document.getElementById("setr");
+var span = document.getElementsByClassName("close3")[0];
+
+btn.onclick = function() {
+    modal3.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal3.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal3) {
+      modal3.style.display = "none";
+    }
+  }
+
+
+var modal4 = document.getElementById("myModal4");
+var btn = document.getElementById("devops");
+var span = document.getElementsByClassName("close4")[0];
+
+btn.onclick = function() {
+    modal4.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal4.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal4) {
+      modal4.style.display = "none";
+    }
+  }

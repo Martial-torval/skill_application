c0966c0694acbe0cd274ee50d2bbbe92abff3852
Martial <Martial.torval@gmail.com>
2022-09-26 10:50:33 +0200

    adjust layout for session page

---
+++ .history/src/Controller/Admin/ExamensCrudController_20220926101856.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Examens;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
+class ExamensCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Examens::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('competences'),
+            te::new('subtitl'),
+            TextField::new('city'),
+            DateTimeField::new('date'),
+        ];
+    }
+    
+}

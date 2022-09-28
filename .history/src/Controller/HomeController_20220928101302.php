8164a05b493d249c4222c61eab9315a486afce71
Martial <Martial.torval@gmail.com>
2022-09-28 13:44:36 +0200

    SQL request for exams per date and limit 4 displayed well in homepage

---
+++ .history/src/Controller/HomeController_20220928101302.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CompetencesRepository;
+use App\Repository\ExamensRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ExamensRepository $repo,CompetencesRepository $repo2): Response
+    {
+        $examens = $repo->findBy(
+            ['date' => 'date' ],
+            ['date' => 'date' ],
+        );
+        $competences = $repo2->findAll();
+        return $this->render('home/index.html.twig',['examens'=>$examens,'competences'=>$competences]);
+    }
+
+  
+
+    #[Route('/session', name: 'session')]
+    public function session(): Response
+    {
+        return $this->render('session/session.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
---
+++ .history/src/Controller/HomeController_20220928112001.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CompetencesRepository;
+use App\Repository\ExamensRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ExamensRepository $repo,CompetencesRepository $repo2): Response
+    {
+        $examens = $repo->findBy(
+            [date_default_timezone_get('') ],
+            ['date' => 'ASC'],
+            limit:4);
+        dd($examens);
+        $competences = $repo2->findAll();
+        return $this->render('home/index.html.twig',['examens'=>$examens,'competences'=>$competences]);
+    }
+
+  
+
+    #[Route('/session', name: 'session')]
+    public function session(): Response
+    {
+        return $this->render('session/session.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}

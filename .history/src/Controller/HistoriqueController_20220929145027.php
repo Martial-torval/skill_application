a7cf49a78b25d16d0774d2c83de97601422f798b
Martial <Martial.torval@gmail.com>
2022-09-29 15:15:24 +0200

    All features required work !

---
+++ .history/src/Controller/HistoriqueController_20220929145027.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Inscription;
+use App\Repository\CompetencesRepository;
+use App\Repository\ExamensRepository;
+use App\Repository\InscriptionRepository;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HistoriqueController extends AbstractController
+{
+    #[Route('/historique/{id}', name: 'historique', methods:'GET')]
+    public function index(ExamensRepository $repo): Response
+    {
+        $user = $this->getUser();
+        $inscriptions = $user->getIdInscriptions();
+
+        $passedExams = $repo->findByDate2()
+        // dd($inscriptions);
+        
+        
+        
+        // $exam = $repo->findAll();
+        
+        
+
+        return $this->render('historique/index.html.twig',['exams'=>$inscriptions]);
+    }
+}
---
+++ .history/src/Controller/SecurityController_20220928151417.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Inscription;
+use App\Entity\User;
+use App\Repository\CompetencesRepository;
+use App\Repository\ExamensRepository;
+use App\Repository\InscriptionRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('user');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/security/user', name: 'user')]
+    public function user(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('security/user.html.twig',['user'=>$user]);
+    }
+
+    #[Route('/user/{id}', methods:['POST'])]
+
+    public function delete( $id, UserRepository $repo, ){
+
+        $user = $repo->find($id);
+        $repo->remove($user, true);
+        $session = new Session();
+        $session->invalidate();
+        
+        return $this->redirectToRoute('login');
+        
+    }
+
+
+     #[Route(path: '/session', name: 'session')]
+    public function examens(ExamensRepository $repo, CompetencesRepository $repo2): Response
+    {           
+
+
+        $examens = $repo->findAll();
+        $competences = $repo2->findAll();
+        $user = $this->getUser();
+       
+        return $this->render('session/session.html.twig',['examens'=>$examens,'competences'=>$competences]);
+    }
+
+
+   #[Route('session/{id}', name: 'show', methods:'POST')]
+   
+public function show($id, ExamensRepository $repo, InscriptionRepository $repo2) {
+    $inscription = new Inscription();
+    $user = $this->getUser();
+
+    // Count nbr registration of current User //
+    $userId = $user->getIdInscriptions();
+    $nbrUserRegistration = $repo2->findBy([
+        
+    ])
+
+      // $currentDate = date('d.m.y');
+        // $examens = $repo->findBy(
+        //     [],
+        //     ['date' => 'ASC'],
+        //     limit:4);
+    dd($nbrUserRegistration);
+
+    $examens = $repo->find($id);
+    $inscription->setUser($user)
+                ->setExamens($examens);
+
+    $repo2->add($inscription, true);
+    
+
+return $this->redirectToRoute('session');
+}
+   
+
+    }
